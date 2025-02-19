73ee5dd57c9b4672008a7b211ce928a32e891a95
MinhKhoiMap <helloiamkhoi@gmail.com>
2025-02-19 16:49:27 +0700

    fasdf

--- api/serverless.js
+++ api/serverless.js
@@ -12,7 +12,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../src/app.ts"));
+app.register(import("../dist/src/app.js"));
 
 export default async (req, res) => {
   await app.ready();
